302ac48da055c7c80bdad6b3fb25866ac0f9fa5a
Avi Chad-Friedman <achadfried@sailthru.com>
2017-01-16 20:56:19 -0500

    chrome extension boiler plate code

---
+++ chrome-extension/app/containers/App.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+@connect(
+  state => ({
+  }),
+  dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+  })
+)
+export default class App extends Component {
+
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+  };
+
+  render() {
+    return (
+      <div className={style.normal}>
+      </div>
+    );
+  }
+}
---
+++ chrome-extension/chrome/extension/inject.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Dock from 'react-dock';
+
+class ReadingApp extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+  }
+}
