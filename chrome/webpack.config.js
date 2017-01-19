bc2c58576b24ba230009a410f429ce36b01cb228
Avi Chad-Friedman <achadfried@sailthru.com>
2017-01-19 12:58:42 -0500

    webpack shits

---
+++ chrome/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack"),
+    path = require("path"),
+    fileSystem = require("fs"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin");
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, "src", "js", "popup.js"),
+    background: path.join(__dirname, "src", "js", "background.js"),
+    manifest: path.join(__dirname, "src", "manifest.json"),
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel" },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup.html"),
+      filename: "popup.html",
+      chunks: ["popup"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "background.html"),
+      filename: "background.html",
+      chunks: ["background"]
+    }),
+    new WriteFilePlugin()
+  ]
+};
